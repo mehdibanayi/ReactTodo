0be0b23e49a87464271777942b7a3618d30a37de
mehdibanayi <meb.kh61@gmail.com>
2022-05-31 17:31:13 +0430

    Navigate

--- src/Routes/Home.js
+++ src/Routes/Home.js
@@ -3,10 +3,11 @@ import TodoList from './../Components/Todo/TodoList';
 import todoApi from './../Api/todos';
 import React ,{useEffect,useState,useContext} from 'react'
 import todosContext from './../Context/todos'
+import { useNavigate } from 'react-router-dom';
 
 export default function Home(){
     const [loading,setLoading]=useState()
-
+    const navigate=useNavigate()
     const todoContext=useContext(todosContext)
 
     let jsonHandler = (data)=>{
@@ -27,12 +28,23 @@ export default function Home(){
         .catch(error=>console.log(error));
     },[])
 
+    const handlerClick = () =>{
+        if(true){
+            navigate('/contact-us')
+        }else{
+            navigate('/about')
+        } 
+    }
+
     return(                                   
         <main>
             <section className="jumbotron">
                 <div className="container d-flex flex-column align-items-center">
                     <h1 className="jumbotron-heading">Welcome!</h1>
                     <p className="lead text-muted">To get started, add some items to your list:</p>
+                        <button onClick={handlerClick}>
+                                Go to Contact US
+                        </button>
                         <FormAddTodo />
                 </div>
             </section>
